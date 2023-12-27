47b259f274fc2b4becc86945bb1ece84c94f7351
pandabeast88 <109696801+pandabeast88@users.noreply.github.com>
2023-12-27 18:36:38 +0800

    Update task_manager.py

--- colab_leecher/utility/task_manager.py
+++ colab_leecher/utility/task_manager.py
@@ -71,10 +71,8 @@ async def taskScheduler():
             elif "drive.google.com" in link:
                 ida = "♻️"
             elif "magnet" in link or "torrent" in link:
-                await cancelTask(
-                    "<i><b>Sorry, But Torrents Are Strictly Prohibited in Google Colab</b>, I Can't allow you because it may shut down the whole project ! Please Find Any alternative Site !</i>"
-                )
-                return
+                 ida = "🧲"
+                Messages.caution_msg = "\n\n⚠️<i><b> Torrents Are Strictly Prohibited in Google Colab</b>, Try to avoid Magnets !</i>"
             elif "youtube.com" in link or "youtu.be" in link:
                 ida = "🏮"
             else:
